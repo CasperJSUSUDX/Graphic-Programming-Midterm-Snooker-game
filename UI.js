b8f030e11956fcdd49e2aaecd725349d8fac950c
CasperJSUSUDX <casper.tw.ma@gmail.com>
2025-12-02 19:00:29 +0800

    switch UI.js's var to private

--- UI.js
+++ UI.js
@@ -1,42 +1,40 @@
 class UI {
-    static UIcontainer;
     static createUIContainer = function() {
-        this.UIcontainer = createDiv();
-        this.UIcontainer.id("ui");
-        this.UIcontainer.position(10, 10);
-        this.UIcontainer.style("display", "flex");
-        this.UIcontainer.style("flex-direction", "column");
+        const UIcontainer = createDiv();
+        UIcontainer.id("ui");
+        UIcontainer.position(10, 10);
+        UIcontainer.style("display", "flex");
+        UIcontainer.style("flex-direction", "column");
     }
     
-    static moveUI;
-    static moveSensetiveText;
-    static moveSlider;
-    static currentSensetiveText;
     static createMoveSensetiveSlider = function(cue) {
-        this.moveUI = createDiv();
-        this.moveUI.id("ui-move");
-        this.moveUI.style("display", "flex");
-        this.moveUI.parent("ui");
-        this.moveSensetiveText = createSpan("Move sensetive: ");
-        this.moveSensetiveText.class("ui-text");
-        this.moveSensetiveText.parent("ui-move");
-        this.moveSlider = createSlider(0, 20, 10);
-        this.moveSlider.size(200);
-        this.moveSlider.parent("ui-move");
-        this.currentSensetiveText = createSpan(this.moveSlider.value());
-        this.currentSensetiveText.class("ui-text");
-        this.currentSensetiveText.parent("ui-move");
-        this.moveSlider.changed(() => {
-            this.currentSensetiveText.html(this.moveSlider.value());
-            cue.adjustSpeed(this.moveSlider.value());
+        const moveUI = createDiv();
+        moveUI.id("ui-move");
+        moveUI.style("display", "flex");
+        moveUI.parent("ui");
+
+        const moveSensetiveText = createSpan("Move sensetive: ");
+        moveSensetiveText.class("ui-text");
+        moveSensetiveText.parent("ui-move");
+
+        const moveSlider = createSlider(0, 20, 10);
+        moveSlider.size(200);
+        moveSlider.parent("ui-move");
+
+        const currentSensetiveText = createSpan(moveSlider.value());
+        currentSensetiveText.class("ui-text");
+        currentSensetiveText.parent("ui-move");
+
+        moveSlider.changed(() => {
+            currentSensetiveText.html(moveSlider.value());
+            cue.adjustSpeed(moveSlider.value());
         });
     }
 
-    static scoreUI;
     static createScoreText = function(score) {
-        this.scoreUI = createSpan(`Score: ${score}`);
-        this.scoreUI.class("ui-text");
-        this.scoreUI.parent("ui");
+        const scoreUI = createSpan(`Score: ${score}`);
+        scoreUI.class("ui-text");
+        scoreUI.parent("ui");
     }
 
     static interval = 60;
