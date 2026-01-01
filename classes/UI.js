d19337b86b55d4ce0ec3938d5fbe8b44db3f512f
Casper <casper.tw.ma@gmail.com>
2026-01-01 17:39:33 +0800

    Remove unused function

--- classes/UI.js
+++ classes/UI.js
@@ -242,14 +242,4 @@ class UI {
   static resetChargeBar() {
     this.#barHeight = this.#height / 2;
   }
-
-  // TODO: Create a GUI
-  static drawHomePage() {
-    push();
-    // Background
-    // Title
-    // Start game
-    // Setting
-    pop();
-  }
 }
